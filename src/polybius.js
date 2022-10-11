5504c24afce48d1a38c6f688b169c12bb4c94b5f
Angele <anjelkayazade@gmail.com>
2022-10-11 19:19:46 -0400

    Wrote the polybius functionalities

--- src/polybius.js
+++ src/polybius.js
@@ -1,14 +1,69 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
-
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-    // your solution code here
+    let result = "";
+    let resultArray = [];
+    let polybius = [['a','f','l','q','v'],
+                    ['b','g','m','r','w'],
+                    ['c','h','n','s','x'],
+                    ['d','(i/j)','o','t','y'],
+                    ['e','k','p','u','z']];
+  let inputArray = input.split('');
+  let first = 0;
+  let second = 0;
+
+    if (encode == false) {
+      let counter = 0;
+      let counterWithSpace = 0;
+      const loop = ()=>{
+          if (inputArray[counterWithSpace] == ' '){
+            counterWithSpace++;
+            resultArray.push(' ');
+          }
+          else if (counter % 2 == 0) {
+            first = inputArray[counterWithSpace] - 1;
+            counter++;
+            counterWithSpace++;
+            second = -1;
+          }
+          else {
+            second = inputArray[counterWithSpace] - 1;
+            counter++;
+            counterWithSpace++;
+            resultArray.push(polybius[first][second])
+          }
+        }
+
+      while (inputArray[counterWithSpace]) {
+        loop(counter,counterWithSpace);
+      }
+    }
+    else {
+      for (let i = 0; i < inputArray.length; i++) {
+        if (inputArray[i] == 'i' || inputArray[i] == 'j') {
+          resultArray.push(4,2);
+        }
+        else if (inputArray[i] != ' ') {
+        loop:  for (let j = 0; j<polybius.length; j++) {
+                for (let k = 0; k<polybius[j].length; k++) {
+                  if (inputArray[i] == polybius[j][k]) {
+                    resultArray.push(j+1,k+1);
+                    break loop;
+                  }
+                }
+              }
+        }
+        else resultArray.push(' ');
+      }
+    }
+
+    result = resultArray.join('');
+    if (second < 0) return false
+    return result;
   }
+
+  polybius("a b c")
 
   return {
     polybius,
