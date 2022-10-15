4717e911d480ff6828da1c0f7f60a4730dc0ae48
Angele <anjelkayazade@gmail.com>
2022-10-15 00:23:04 -0400

    substitution function written

--- src/substitution.js
+++ src/substitution.js
@@ -7,9 +7,59 @@ const substitutionModule = (function () {
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    if (!alphabet || alphabet.length != 26) return false;
+    
+    let result = '';
+    let lowercaseInput = input.toLowerCase();
+    let inputArray = input.split('');
+    let inputAlphabetArray = alphabet.split('');
+    let resultArray = [];
+    let alphabetRef = 'abcdefghijklmnopqrstuvwxyz';
+
+    
+    const uniqueCharacters = inputAlphabetArray.filter((item, index, self) => 
+    self.indexOf(item) === index);
+    
+    if (uniqueCharacters.length != alphabet.length) return false;
+  
+
+   const encoding  = () => {
+
+    const encode = (char) => {
+      const charIndex = alphabetRef.indexOf(char);
+      const letter = alphabet[charIndex];
+      resultArray.push(letter);
+    }
+
+    inputArray.forEach(char => {
+      char == ' ' ? resultArray.push(' ') : resultArray.push(encode(char));
+        })
+
+    return resultArray.join('');
+   }
+
+   const decoding = () => {
+
+    const decode = (char) => {
+      const charIndex = alphabet.indexOf(char);
+      const letter = alphabetRef[charIndex];
+      resultArray.push(letter);
+    }
+
+    inputArray.forEach(char => {
+      char == ' ' ? resultArray.push(' ') : resultArray.push(decode(char));
+        })
+
+    return resultArray.join('');
+   }
+    
+    
+   return encode ? encoding() : decoding();
+
   }
 
+
+  substitution("thinkful", "xoxqmcgrukswaflnthdjpzibev");
   return {
     substitution,
   };
